b729cd92f9800d12d880e99c611e42536d846919
npc-xss <119160390+npc-xss@users.noreply.github.com>
2023-08-02 02:21:35 +0545

    removed next-auth provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "@/hooks/use-cart";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <CartProvider>{children}</CartProvider>
-    </SessionProvider>
-  );
+  return <CartProvider>{children}</CartProvider>;
 }
